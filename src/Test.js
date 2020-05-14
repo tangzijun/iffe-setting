c61f4a9a5ee7ec264b512d2f8ce56bda8ff86411
唐子君 <223663737@qq.com>
2020-05-14 19:43:37 +0800

    test: jsdh
    jasd

--- src/Test.js
+++ src/Test.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef } from "react";
 export default (props) => {
   const { url } = props;
   const imageRef = useRef();
+
   useEffect(() => {
     if (url && imageRef.current) {
       imageRef.current.setAttribute("src", url);
