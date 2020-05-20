47e407f18135d1fec6b03f7661ed2aa81b0f566f
guokai <guokaigdg@gmail.com>
2020-05-20 12:22:53 +0800

    路由hashRouter测试

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import "typeface-roboto"; // Google字体
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { Route } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { SettingView } from "./components/setting/index";
 import getAppTheme from "./app-theme";
 import { LogIn } from "./components/login";
+// import { Button } from "solidoc-base-component-lib";
 
 function App() {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -26,22 +25,24 @@ function App() {
     <ThemeProvider theme={theme}>
       <Router>
         <div>
-          <Link to={"./login"}></Link>
-          <Link to={"./setting"}></Link>
-          <Link to={"./iffe-setting"}></Link>
+          <Link to={"/login"}></Link>
+          <Link to={"/setting"}></Link>
+          <Link to={"/iffe-setting"}></Link>
         </div>
-        <Route path="/" exact>
-          <LogIn />
-        </Route>
-        <Route path="/iffe-setting" exact>
-          <SettingView />
-        </Route>
-        <Route path="/login" exact>
-          <LogIn />
-        </Route>
-        <Route path="/setting" exact>
-          <SettingView />
-        </Route>
+        <div>
+          <Route path="/" exact>
+            <LogIn />
+          </Route>
+          <Route path="/iffe-setting" exact>
+            <LogIn />
+          </Route>
+          <Route path="/login" exact>
+            <LogIn />
+          </Route>
+          <Route path="/setting" exact>
+            <SettingView />
+          </Route>
+        </div>
       </Router>
     </ThemeProvider>
   );
--- src/components/setting/setting-content/index.jsx
+++ src/components/setting/setting-content/index.jsx
@@ -10,7 +10,7 @@ import { ShortcutKey } from "./view/ShortcutKey";
 import { MarkdownShortcutkey } from "./view/MarkdownShortcutkey";
 import { UpdateLog } from "./view/UpdateLog";
 import { ConnectWithUs } from "./view/ConnectWithUs";
-import { LogIn } from "../../login/index";
+
 function ToUserSettingsAccount() {
   return (
     <div>
@@ -25,68 +25,63 @@ function SettingContent() {
         {/*  社设置默认页面我的账户 */}
         <Route path="/setting" component={ToUserSettingsAccount} exact />
         {/* 我的账户 */}
-        <Route exact path="/usersettingsaccount">
+        <Route exact path="/setting/usersettingsaccount">
           <div>
             <UserSettingsAccount />
           </div>
         </Route>
         {/* 隐私与安全 */}
-        <Route exact path="/privacyandsecurity">
+        <Route exact path="/setting/privacyandsecurity">
           <div>
             <PrivacyAndSecurity />
           </div>
         </Route>
         {/* 方案与付款 */}
-        <Route exact path="/schemeandpayment">
+        <Route exact path="/setting/schemeandpayment">
           <div>
             <SchemeAndPayment />
           </div>
         </Route>
         {/* 语言 */}
-        <Route exact path="/language">
+        <Route exact path="/setting/language">
           <div>
             <Language />
           </div>
         </Route>
         {/* 外观 */}
-        <Route exact path="/theme">
+        <Route exact path="/setting/theme">
           <div>
             <Theme />
           </div>
         </Route>
         {/* 通知 */}
-        <Route exact path="/message">
+        <Route exact path="/setting/message">
           <div>
             <Message />
           </div>
         </Route>
         {/* 快捷键 */}
-        <Route exact path="/shortcutkey">
+        <Route exact path="/setting/shortcutkey">
           <div>
             <ShortcutKey />
           </div>
         </Route>
         {/* Markdown快捷键 */}
-        <Route exact path="/markdownkey">
+        <Route exact path="/setting/markdownkey">
           <div>
             <MarkdownShortcutkey />
           </div>
         </Route>
         {/* 更新日志 */}
-        <Route exact path="/updatelog">
+        <Route exact path="/setting/updatelog">
           <div>
             <UpdateLog />
           </div>
         </Route>
         {/* 联系我们 */}
-        <Route exact path="/connectwithus">
+        <Route exact path="/setting/connectwithus">
           <div>
             <ConnectWithUs />
-          </div>
-        </Route>
-        <Route exact path="/login">
-          <div>
-            <LogIn />
           </div>
         </Route>
       </Switch>
